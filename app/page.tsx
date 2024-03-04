d3cba7e51cc106e2431d6d0ebb39a843cfd7b793
Francesco Culmone <fculmone5@gmail.com>
2024-03-04 14:18:53 -0500

    added suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
-import ClanTagInput from "./ui/clanTagInput";
 import BasicTable from "./ui/table";
-import mData from './sample-clan-data.json';
-import TableWithClanInput from "./ui/tableWithClanInput";
 import { getDataFromServer } from "./lib/data";
+import { RevenueChartSkeleton } from "./ui/skeletons";
+import { Suspense } from "react";
 
 export default function Home() {
-  
-  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <BasicTable getData={getDataFromServer} />
+      <Suspense>
+        <BasicTable getData={getDataFromServer} />
+      </Suspense> 
       <p className="text-blue-900">heheheha</p>
     </main>
   );
