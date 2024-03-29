fa0f094c87aa828182a7c40f1719bb430976d6a3
Francesco Culmone <fculmone5@gmail.com>
2024-03-29 16:23:58 -0400

    handled 500 errors

--- app/ui/table.tsx
+++ app/ui/table.tsx
@@ -69,22 +69,25 @@ export default function BasicTable({ getData }: {getData: any}) {
             setIsLoading(false);
           } else {
             //await new Promise((resolve) => setTimeout(resolve, 3000));
-            try {
-              const initialData = await getData(searchParams.get('clan-tag')?.toString());
-              console.log('hi');
-              setData(initialData);
-            } catch (e) {
-              console.log(e);
-              console.log('caught above error in use effect try catch')
-            }
-            
+            const initialData = await getData(searchParams.get('clan-tag')?.toString());
+            console.log('hi');
+            setData(initialData);
             setIsLoading(false);
           }
           
         }
         
-      } catch (err) {
-        console.error(err);
+      } catch (err: any) {
+        if (err.response) { // if there is response, it means its not a 50x, but 4xx
+          console.error(err);
+          console.log('should only go on 400 errors');
+          throw err;
+        } else {   // gets activated on 50x errors, since no response from server
+          // do whatever you want here :)
+          console.error(err);
+          console.log('should be a 500 timeout error. restarting page');
+          location.reload();
+        }       
       }
     })();
   }, []);
