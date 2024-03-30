ff4cc9d257d6b5dc9eb0abf2b194532405cff84e
Francesco Culmone <fculmone5@gmail.com>
2024-03-30 14:40:31 -0400

    added try catch to data portion

--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -4,8 +4,21 @@ export async function getDataFromServer(clanTag: string) {
   'use server'
   let data;
   //console.log(process.env.BASE_URL + '/get-clan/' + clanTag)
-  const result = await fetch(process.env.BASE_URL + '/get-clan/' + clanTag);
-    
-  data = await result.json();
+  try {
+    const result = await fetch(process.env.BASE_URL + '/get-clan/' + clanTag);   
+    data = await result.json();
+  } catch (err: any) {
+    if (err.response) { // if there is response, it means its not a 50x, but 4xx
+      console.error(err);
+      console.log('should only go on 400 errors');
+      throw err;
+    } else {   // gets activated on 50x errors, since no response from server
+      // do whatever you want here :)
+      console.error(err);
+      console.log('should be a 500 timeout error. restarting page');
+      location.reload();
+    }    
+  }
+  
   return data;
 }
