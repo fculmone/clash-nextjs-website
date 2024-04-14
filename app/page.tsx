514be63ad10d57a0f41342d897cfa2a537034ecb
Francesco Culmone <fculmone5@gmail.com>
2024-04-14 01:04:46 -0400

    wrapped header in suspense so it will build

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Header from "./ui/header";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between px-24">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       <p className="text-red-900">heheheha</p>
     </div>
   );
--- app/war-stats/page.tsx
+++ app/war-stats/page.tsx
@@ -1,27 +1,12 @@
+import { Suspense } from "react";
 import Header from "../ui/header";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center px-24">
-      {/* prettier-ignore */}
-      <Header />
-      <div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣛⡉⢹⢛⣛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⡟⣫⡆⠀⣿⣷⣬⣼⡿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⣷⡘⢿⣾⡿⠿⠿⠛⢃⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣌⠫⠶⠿⠟⣋⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⡏⠉⠏⠩⣭⣭⠉⠘⠉⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⡇⠠⠤⠴⢯⣭⢤⠤⢄⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⠋⠀⠀⠶⠬⢼⣸⣇⢠⠬⠼⠌⠀⠀⢹⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠙⠙⠛⠉⠀⠀⠀⠀⠠⣾⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⠏⠀⢀⣮⣅⣛⠻⠇⠿⢛⣃⣩⣴⠃⠀⠈⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣧⡀⠀⠘⢿⣿⠿⠟⠻⠟⣛⠻⢿⠿⠀⠀⣼⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⢀⠀⠀⠲⠈⣭⣭⢩⣭⡴⠐⠀⠀⡌⣿⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⠇⣾⣷⣦⣀⠀⠀⠈⠀⠀⠀⣠⣴⣿⣿⡸⣿⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⡏⣼⣿⣿⣿⣿⣿⣶⣤⣀⣴⣿⣿⣿⣿⣿⣧⢹⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿</div>
-        <div className="my-0">⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿</div>
-      </div>
+      <Suspense>
+        <Header />
+      </Suspense>
     </main>
   );
 }
