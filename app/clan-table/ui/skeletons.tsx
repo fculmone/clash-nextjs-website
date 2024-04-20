d856f78481c503b001ea0d457d55e960dc995b8a
Francesco Culmone <fculmone5@gmail.com>
2024-04-20 15:17:28 -0400

    trying to fix skeleton rounded corner overflow

--- app/clan-table/ui/skeletons.tsx
+++ app/clan-table/ui/skeletons.tsx
@@ -6,19 +6,21 @@ export function TableSkeleton() {
   return (
     <>
       <div className="text-center mt-12">
-        <SkeletonTheme baseColor="#b9b9b9" highlightColor="#999999">
-          <Skeleton className=" max-w-[300px]" height={40} />
-        </SkeletonTheme>
+        <div className="w-full h-full overflow-hidden">
+          <SkeletonTheme baseColor="#b9b9b9" highlightColor="#999999">
+            <Skeleton className=" max-w-[300px]" height={40} />
+          </SkeletonTheme>
+        </div>
       </div>
       <div className="xl:w-[1200px] lg:w-[1000px] w-screen bg-gray-200 border-2 border-solid border-gray-400 h-96 py-2 px-4 rounded-lg mt-20">
         <SkeletonTheme baseColor="#b9b9b9" highlightColor="#999999">
-          <Skeleton className="mt-2 mb-2" height={60} />
-          <Skeleton className="my-1" height={40} />
-          <Skeleton className="my-1" height={40} />
-          <Skeleton className="my-1" height={40} />
-          <Skeleton className="my-1" height={40} />
-          <Skeleton className="my-1" height={40} />
-          <Skeleton className="my-1" height={40} />
+          <Skeleton className="mt-2 mb-2 overflow-hidden" height={60} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
+          <Skeleton className="my-1 overflow-hidden rounded-lg" height={40} />
         </SkeletonTheme>
       </div>
     </>
